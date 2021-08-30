93d044151de7c5f22e1916e723f41b654f09f830
Hugo Gallardo <hugo.gallardo@jaak-it.com>
2021-08-30 12:51:50 -0500

    Addin java logic to android example usage project for plugin implementation

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,5 +1,24 @@
 package io.ionic.starter;
-import com.jaakrecog.fingerprint;
-import com.getcapacitor.BridgeActivity;
+import android.os.Bundle;
 
-public class MainActivity extends BridgeActivity {}
+import com.getcapacitor.Plugin;
+import com.jaakrecog.fingerprint.FingerPrint;
+import com.getcapacitor.BridgeActivity;
+import com.jaakrecog.fingerprint.FingerPrintPlugin;
+
+import java.util.ArrayList;
+
+public class MainActivity extends BridgeActivity {
+
+@Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    // Initializes the Bridge
+    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
+      // Additional plugins you've installed go here
+      add(FingerPrintPlugin.class);
+
+    }});
+  }
+}
