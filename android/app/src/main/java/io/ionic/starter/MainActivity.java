e24f817372475714956d26129235f9c74baa7e30
Hugo Gallardo <hugo.gallardo@jaak-it.com>
2021-08-30 16:32:20 -0500

    Trying demo token

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -13,12 +13,6 @@ public class MainActivity extends BridgeActivity {
 @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-
-    // Initializes the Bridge
-    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
-      // Additional plugins you've installed go here
-      add(FingerPrintPlugin.class);
-
-    }});
+    registerPlugin(FingerPrintPlugin.class);
   }
 }
