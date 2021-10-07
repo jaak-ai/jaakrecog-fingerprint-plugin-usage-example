0d3911830429f6bec30560d74f1370fc2f6a6bfb
Javier Moya <javier.moya@jaak-it.com>
2021-10-07 12:21:03 -0500

    feat: add sentry monitoring

--- android/app/src/main/java/com/jaakrecog/fingerprintusage/MainActivity.java
+++ android/app/src/main/java/com/jaakrecog/fingerprintusage/MainActivity.java
@@ -1,21 +1,25 @@
 package com.jaakrecog.fingerprintusage;
+
 import android.os.Bundle;
 import android.util.Log;
 
 import com.getcapacitor.BridgeActivity;
 import com.jaakrecog.fingerprint.FingerPrintPlugin;
 
+import io.sentry.capacitor.SentryCapacitor;
+
 
 public class MainActivity extends BridgeActivity {
 
-@Override
+  @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    try{
-
+    try {
+      registerPlugin(SentryCapacitor.class);
       registerPlugin(FingerPrintPlugin.class);
-    }catch (Exception ex){
-      Log.e("Plugin exception",ex.getMessage());
+
+    } catch (Exception ex) {
+      Log.e("Plugin exception", ex.getMessage());
     }
   }
 }
